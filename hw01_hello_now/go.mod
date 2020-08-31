d1ff7698cedccf28586aa7bc7fe7ceedc7ac4601
Karasev <karasevaa@tecon.ru>
2020-08-31 11:32:15 +0300

    GOLANG-1 работа 1 выполнена

--- hw01_hello_now/go.mod
+++ hw01_hello_now/go.mod
@@ -1,10 +1,11 @@
-module github.com/fixme_my_friend/hw01_hello_now
+module github.com/alexandr-kaa/otus_golang_hw/hw01_hello_now
 
 go 1.14
 
 require (
 	bou.ke/monkey v1.0.2
-	github.com/beevik/ntp v0.2.0
-	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	github.com/beevik/ntp v0.3.0
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
+	golang.org/x/tools v0.0.0-20200828161849-5deb26317202 // indirect
 )
--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	// layout:="09 Jan 2006 01:02:01 +0000 UTC"
+	currTime := time.Now()
+	timeExact, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err != nil {
+		log.Fatalf("Обнаружена ошибка при обращении к ntp server:\n " + err.Error())
+	}
+
+	currTime = currTime.Round(0)
+	timeExact = timeExact.Round(0)
+
+	// fmt.Println("```text")
+	fmt.Println("current time: " + currTime.String())
+	fmt.Println("exact time: " + timeExact.String())
+	// fmt.Println("```")
 }
