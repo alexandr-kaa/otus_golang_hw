d1170b7c8cfc7c2e819af6171212e05c5985ee83
Alexandr Karasev <dlavstrech@mail.ru>
2020-11-08 14:04:21 +0300

    GOLANG-7 первая проба 7 ДЗ

--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 )
 
 var (
@@ -19,4 +20,8 @@ func init() {
 func main() {
 	flag.Parse()
 	// Place your code here
+
+	if err := Copy(from, to, offset, limit); err != nil {
+		log.Fatalf(err.Error())
+	}
 }
